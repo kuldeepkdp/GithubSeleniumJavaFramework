91c112c182fff30f7166aad7bc3fadcf56118988
kuldeep kumar <kuldeep.kdp@gmail.com>
2018-04-05 17:50:19 +0530

    Added test cases for Contact and deleted test cases for account

---
+++ SeleniumJavaFramework/src/test/java/com/kdp/SeleniumJavaFramework/TestCases/PBI_102_Contact.java
@@ -0,0 +1,34 @@
+package com.kdp.SeleniumJavaFramework.TestCases;
+
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.kdp.SeleniumJavaFramework.BaseFunction.Selenium;
+import com.kdp.SeleniumJavaFramework.Pages.HomePage;
+
+public class PBI_102_Contact {
+	
+	@BeforeMethod(alwaysRun = true)
+	public void before() throws IOException, InterruptedException {
+		Selenium.openBrowser();
+	}
+	
+	@Test(groups = { "smoke" })
+	public void testValidationOnContactForm() throws InterruptedException,
+			IOException, InvalidFormatException {
+		
+		System.out.println("Write your Test Code here");
+
+	}
+	
+	@AfterMethod (alwaysRun= true)
+	public void after() throws IOException, InterruptedException {
+		Selenium.driver.quit();
+	}
+
+}
