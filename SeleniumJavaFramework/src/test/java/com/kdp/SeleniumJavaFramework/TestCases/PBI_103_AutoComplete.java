2316296b5cce84352f31c54c827632e86fecceb8
lenovo <lenovo@LAPTOP-JO88T3FI>
2018-04-13 22:39:16 +0530

    Added script to test autocomplete.

---
+++ SeleniumJavaFramework/src/test/java/com/kdp/SeleniumJavaFramework/TestCases/PBI_103_AutoComplete.java
@@ -0,0 +1,36 @@
+package com.kdp.SeleniumJavaFramework.TestCases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.kdp.SeleniumJavaFramework.BaseFunction.Selenium;
+import com.kdp.SeleniumJavaFramework.Pages.AutoCompletePage;
+import com.kdp.SeleniumJavaFramework.Pages.ContactPage;
+
+public class PBI_103_AutoComplete {
+	@BeforeMethod(alwaysRun = true)
+	public void before() throws IOException, InterruptedException {
+		Selenium.openBrowser();
+	}
+	
+	//This test script is to test auto complete filed- only navigation
+	@Test(enabled = true, groups = { "smoke" })
+	public void testAutoComplete() throws InterruptedException {
+		Selenium.driver.findElement(AutoCompletePage.autoselectlink).click();
+		Selenium.driver.findElement(AutoCompletePage.tag).sendKeys("ja");
+		Thread.sleep(5000);
+		Selenium.driver.findElement(AutoCompletePage.tagname).click();
+
+	}
+	
+	@AfterMethod (alwaysRun= true)
+	public void after() throws IOException, InterruptedException {
+	Selenium.driver.close();
+	}
+
+}
